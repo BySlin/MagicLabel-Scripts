467d4c2b14a81de4ebb9ba7ef16665e90e336cdc
WrOve <2292735470@qq.com>
2025-07-11 01:24:00 +0800

    feat: 更新版本

--- app/common.py
+++ app/common.py
@@ -1,8 +1,7 @@
 import os
-import traceback
 from multiprocessing import Queue, Pipe, Manager
 from pathlib import Path
-from typing import Union
+from typing import Union, TYPE_CHECKING
 
 from custom import custom_framework
 from lib.SSE import ServerSentEvents
@@ -20,32 +19,41 @@ models_directory = os.path.join(current_directory, "..")
 
 # 加载ultralytics是否成功
 load_ultralytics_success = module_exists("ultralytics")
+print("[MagicLabel]load_ultralytics_success:", load_ultralytics_success)
 # 加载yolov5是否成功
 load_yolov5_success = module_exists("yolov5")
+print("[MagicLabel]load_yolov5_success:", load_yolov5_success)
 # 加载sahi是否成功
 load_sahi_success = module_exists("sahi")
+print("[MagicLabel]load_sahi_success:", load_sahi_success)
 # 加载SAM2是否成功
 load_sam2_success = module_exists("sam2")
+print("[MagicLabel]load_sam2_success:", load_sam2_success)
+
+if TYPE_CHECKING:
+  from ultralytics import FastSAM, SAM, YOLOE, YOLOWorld
+  from sam2.sam2_video_predictor import SAM2VideoPredictor
+  from sam2.sam2_image_predictor import SAM2ImagePredictor
 
 # SSE事件
 sse_events = ServerSentEvents()
 
 # 多进程管理器
-manager = None
+manager: Union[Manager, None] = None
 # 推理消息
-predict_msg_queue = None
+predict_msg_queue: Union[Queue, None] = None
 
 # 推理进程
-predict_process = None
+predict_process: Union[ProcessMsgThread, None] = None
 # 推理进程消息通道
 predict_main_conn, predict_process_conn = Pipe()
 
 # SAM模型
-sam_model = None
+sam_model: Union["FastSAM", "SAM", "YOLOE", "YOLOWorld", None] = None
 
 # SAM2模型
-sam2_video_predictor = None
-sam2_image_predictor = None
+sam2_video_predictor: Union["SAM2VideoPredictor", None] = None
+sam2_image_predictor: Union["SAM2ImagePredictor", None] = None
 
 def initialize():
   global sse_events, manager, predict_msg_queue, predict_process, predict_process_conn, sam_model, load_ultralytics_success
