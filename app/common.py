8d92172133bff13defcad4757d30ab5525c1ff4e
WrOve <2292735470@qq.com>
2025-07-11 01:18:39 +0800

    feat: 更新版本

--- app/common.py
+++ app/common.py
@@ -7,6 +7,7 @@ from typing import Union
 from custom import custom_framework
 from lib.SSE import ServerSentEvents
 from lib.process_msg_thread import ProcessMsgThread
+from lib.utils import module_exists
 from yolo_task import predict_task_process
 
 # 加载自定义包
@@ -18,83 +19,33 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 models_directory = os.path.join(current_directory, "..")
 
 # 加载ultralytics是否成功
-load_ultralytics_success = False
+load_ultralytics_success = module_exists("ultralytics")
 # 加载yolov5是否成功
-load_yolov5_success = False
+load_yolov5_success = module_exists("yolov5")
 # 加载sahi是否成功
-load_sahi_success = False
-
-try:
-  from ultralytics import YOLO
-
-  load_ultralytics_success = True
-except ImportError:
-  YOLO = None
-  load_ultralytics_success = False
-
-try:
-  from ultralytics import FastSAM
-except ImportError:
-  FastSAM = None
-
-try:
-  from ultralytics import SAM
-except ImportError:
-  SAM = None
-
-try:
-  from ultralytics import YOLOE
-except ImportError:
-  YOLOE = None
-
-try:
-  from ultralytics import YOLOWorld
-except ImportError:
-  YOLOWorld = None
-
-try:
-  import yolov5
-
-  load_yolov5_success = True
-except ImportError:
-  load_yolov5_success = False
-
-try:
-  from sahi import AutoDetectionModel
-
-  load_sahi_success = True
-except ImportError:
-  load_sahi_success = False
-
-try:
-  from sam2.build_sam import build_sam2_video_predictor, build_sam2
-  from sam2.sam2_video_predictor import SAM2VideoPredictor
-  from sam2.sam2_image_predictor import SAM2ImagePredictor
-except ImportError:
-  build_sam2 = None
-  build_sam2_video_predictor = None
-  SAM2VideoPredictor = None
-  SAM2ImagePredictor = None
+load_sahi_success = module_exists("sahi")
+# 加载SAM2是否成功
+load_sam2_success = module_exists("sam2")
 
 # SSE事件
 sse_events = ServerSentEvents()
 
 # 多进程管理器
-manager: Union[Manager, None] = None
+manager = None
 # 推理消息
-predict_msg_queue: Union[Queue, None] = None
+predict_msg_queue = None
 
 # 推理进程
-predict_process: Union[ProcessMsgThread, None] = None
+predict_process = None
 # 推理进程消息通道
 predict_main_conn, predict_process_conn = Pipe()
 
 # SAM模型
-sam_model: Union[FastSAM, SAM, YOLOE, YOLOWorld, None] = None
+sam_model = None
 
 # SAM2模型
-sam2_video_predictor: Union[SAM2VideoPredictor, None] = None
-sam2_image_predictor: Union[SAM2ImagePredictor, None] = None
+sam2_video_predictor = None
+sam2_image_predictor = None
 
 def initialize():
   global sse_events, manager, predict_msg_queue, predict_process, predict_process_conn, sam_model, load_ultralytics_success
@@ -124,6 +75,10 @@ def destroy():
 def set_sam_model(model_path):
   global sam_model, load_ultralytics_success
   if load_ultralytics_success:
+    try:
+        from ultralytics import FastSAM, SAM, YOLOE
+    except ImportError:
+        return False
     stem = Path(model_path).stem.lower()
     if "fastsam" in stem:
       if FastSAM is not None:
@@ -142,7 +97,11 @@ def set_sam_model(model_path):
 
 def set_sam2_video_model(config_file, model_path):
   global sam2_video_predictor
-  if build_sam2_video_predictor is not None:
+  if load_sam2_success:
+    try:
+      from sam2.build_sam import build_sam2_video_predictor
+    except ImportError:
+      return False
     sam2_video_predictor = build_sam2_video_predictor(config_file, model_path)
     return True
   return False
@@ -150,7 +109,11 @@ def set_sam2_video_model(config_file, model_path):
 
 def set_sam2_image_model(config_file, model_path):
   global sam2_image_predictor
-  if build_sam2 is not None:
+  if load_sam2_success:
+    try:
+      from sam2.build_sam import build_sam2
+    except ImportError:
+      return False
     sam2_image_predictor = build_sam2(config_file, model_path)
     return True
   return False
